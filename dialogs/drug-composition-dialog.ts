567c86af38fafc37a602d86de2a7332a5d7bca31
Jean-Sébastien Dupuy <js.dupuy@live.com>
2018-05-02 11:46:57 +0200

    updating drug composition dialog

--- dialogs/drug-composition-dialog.ts
+++ dialogs/drug-composition-dialog.ts
@@ -1,41 +1,110 @@
 import * as builder from 'botbuilder';
 import openmedicament from "../services/api-openmedicaments";
+import { Medication, MedicationCode } from "../services/api-openmedicaments";
 
-let lib = new builder.Library('drug');
+let lib = new builder.Library('medication');
 
 // Extends IEntity interface to support entity resolution.
 declare interface IEntityEx extends builder.IEntity {
     resolution: any;
 }
 
+interface IStoredData {
+    name: string
+    search: MedicationCode[]|undefined
+}
+
 lib.dialog('composition', [
     async (session: builder.Session, args: any, next?: (results?: builder.IDialogResult<any>) => void) => {
-        let drug: any;
+        let medication: IStoredData|undefined = undefined;
 
+        // Get the Medication entity
         if (args && args.intent) {
             let intent = args.intent;
-            let medication: IEntityEx = builder.EntityRecognizer.findEntity(intent.entities, 'Medication') as IEntityEx;
+            let medicationEntity: IEntityEx = builder.EntityRecognizer.findEntity(intent.entities, 'Medication') as IEntityEx;
 
-            drug = session.dialogData.drug = {
-                name: medication ? medication.entity : undefined
+            medication = session.dialogData.drug = {
+                name: medicationEntity ? medicationEntity.entity : undefined,
+                search : undefined
             }
         }
 
-        if (drug && drug.name) {
-            let codes = await openmedicament.getMedicationCodeFromQueryAsync(drug.name);
-            let info = await openmedicament.getMedicationFromIdAsync(codes[0].codeCIS);
+        if (medication && medication.name) {
+            if (next) next();
+        }
+        else {
+            builder.Prompts.text(session, "What is the medication name ?")
+        }
+    },
+    async (session: builder.Session, results: any, next?: (results?: builder.IDialogResult<any>) => void) => {
+        let medication: IStoredData = session.dialogData.drug;
+        if (results && results.response) {
+            medication = session.dialogData.drug = {
+                name : results.response,
+                search : undefined
+            }
+        }
 
-            for(var composition of info.compositions) {
-                for(var substance of composition.substancesActives) {
-                    session.send(substance.denominationSubstance);
+        if (medication && medication.name) {
+            try {
+                let listOfMedication = await openmedicament.getMedicationCodeFromQueryAsync(medication.name);
+                medicationPrompt(session, listOfMedication, next);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    },
+    async (session: builder.Session, results: any, next?: (results?: builder.IDialogResult<any>) => void) => {
+        let medication: IStoredData = session.dialogData.drug;
+
+        // Formatting the response
+        if (results && results.response && results.response.entity && medication.search) {
+            let response = results.response.entity;
+
+            for (let item of medication.search) {
+                if (item.denomination == response) {
+                    let drug = await openmedicament.getMedicationFromIdAsync(item.codeCIS);
+
+                    for(var composition of drug.compositions) {
+                        for(var substance of composition.substancesActives) {
+                            session.send(`${substance.denominationSubstance} with a dosage of ${substance.dosageSubstance}`);
+                        }
+                    }
                 }
             }
         }
-    
+
         session.endDialog();
     }
 ]).triggerAction({ matches: 'Intent.Medication.GetComposition' });
 
-export function createLibrary() : builder.Library {
+
+function medicationPrompt(session: builder.Session, search: MedicationCode[], next?: (results?: builder.IDialogResult<any>) => void) {
+
+    let medications = new Array<string>();
+
+    if (search && search.length > 0) {
+        // Limit the result to the first ten answers
+        if (search.length > 10) {
+            search = search.slice(0, 10);
+        }
+
+        // Save the current search in dialogData state
+        session.dialogData.drug.search = search;
+
+        for (let item of search) {
+            medications.push(item.denomination);
+        }
+
+        builder.Prompts.choice(session, "Please confirm the name of the medication:", medications);
+    }
+    else {
+
+        session.send("Sorry. I didn't find any matching medication.")
+        if (next) next();
+    }
+}
+
+export function createLibrary(): builder.Library {
     return lib.clone();
 }
--- out/dialogs/drug-composition-dialog.js
+++ out/dialogs/drug-composition-dialog.js
@@ -10,29 +10,84 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const builder = require("botbuilder");
 const api_openmedicaments_1 = require("../services/api-openmedicaments");
-let lib = new builder.Library('drug');
+let lib = new builder.Library('medication');
 lib.dialog('composition', [
     (session, args, next) => __awaiter(this, void 0, void 0, function* () {
-        let drug;
+        let medication = undefined;
+        // Get the Medication entity
         if (args && args.intent) {
             let intent = args.intent;
-            let medication = builder.EntityRecognizer.findEntity(intent.entities, 'Medication');
-            drug = session.dialogData.drug = {
-                name: medication ? medication.entity : undefined
+            let medicationEntity = builder.EntityRecognizer.findEntity(intent.entities, 'Medication');
+            medication = session.dialogData.drug = {
+                name: medicationEntity ? medicationEntity.entity : undefined,
+                search: undefined
             };
         }
-        if (drug && drug.name) {
-            let codes = yield api_openmedicaments_1.default.getMedicationCodeFromQueryAsync(drug.name);
-            let info = yield api_openmedicaments_1.default.getMedicationFromIdAsync(codes[0].codeCIS);
-            for (var composition of info.compositions) {
-                for (var substance of composition.substancesActives) {
-                    session.send(substance.denominationSubstance);
+        if (medication && medication.name) {
+            if (next)
+                next();
+        }
+        else {
+            builder.Prompts.text(session, "What is the medication name ?");
+        }
+    }),
+    (session, results, next) => __awaiter(this, void 0, void 0, function* () {
+        let medication = session.dialogData.drug;
+        if (results && results.response) {
+            medication = session.dialogData.drug = {
+                name: results.response,
+                search: undefined
+            };
+        }
+        if (medication && medication.name) {
+            try {
+                let listOfMedication = yield api_openmedicaments_1.default.getMedicationCodeFromQueryAsync(medication.name);
+                medicationPrompt(session, listOfMedication, next);
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+    }),
+    (session, results, next) => __awaiter(this, void 0, void 0, function* () {
+        let medication = session.dialogData.drug;
+        // Formatting the response
+        if (results && results.response && results.response.entity && medication.search) {
+            let response = results.response.entity;
+            for (let item of medication.search) {
+                if (item.denomination == response) {
+                    let drug = yield api_openmedicaments_1.default.getMedicationFromIdAsync(item.codeCIS);
+                    for (var composition of drug.compositions) {
+                        for (var substance of composition.substancesActives) {
+                            session.send(`${substance.denominationSubstance} with a dosage of ${substance.dosageSubstance}`);
+                        }
+                    }
                 }
             }
         }
         session.endDialog();
     })
 ]).triggerAction({ matches: 'Intent.Medication.GetComposition' });
+function medicationPrompt(session, search, next) {
+    let medications = new Array();
+    if (search && search.length > 0) {
+        // Limit the result to the first ten answers
+        if (search.length > 10) {
+            search = search.slice(0, 10);
+        }
+        // Save the current search in dialogData state
+        session.dialogData.drug.search = search;
+        for (let item of search) {
+            medications.push(item.denomination);
+        }
+        builder.Prompts.choice(session, "Please confirm the name of the medication:", medications);
+    }
+    else {
+        session.send("Sorry. I didn't find any matching medication.");
+        if (next)
+            next();
+    }
+}
 function createLibrary() {
     return lib.clone();
 }
