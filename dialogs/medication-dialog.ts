9696f15c57ec9029c215af71c78a4fc9308d29b5
Jean-Sébastien Dupuy <js.dupuy@live.com>
2018-05-03 14:42:04 +0200

    adding dialog for Intent.Medications.Translate

--- dialogs/medication-dialog.ts
+++ dialogs/medication-dialog.ts
@@ -2,6 +2,7 @@ import * as builder from 'botbuilder';
 import openmedicament from "../services/api-openmedicaments";
 import { Medication, MedicationCode } from "../services/api-openmedicaments";
 import translator from "../services/cognitive-translator";
+import medicationcard from "../cards/medication-card"
 
 let lib = new builder.Library('medication');
 
@@ -10,14 +11,21 @@ declare interface IEntityEx extends builder.IEntity {
     resolution: any;
 }
 
-interface IStoredData {
+interface IInformationStoredData {
     name: string
+    search: MedicationCode[]|undefined
+}
+
+interface ITranslateStoredData {
+    name: string
+    language: string
+    languageCode: string
     search: MedicationCode[]|undefined
 }
 
 lib.dialog('information', [
     async (session: builder.Session, args: any, next?: (results?: builder.IDialogResult<any>) => void) => {
-        let medication: IStoredData|undefined = undefined;
+        let medication: IInformationStoredData|undefined = undefined;
 
         // Get the Medication entity
         if (args && args.intent) {
@@ -38,7 +46,7 @@ lib.dialog('information', [
         }
     },
     async (session: builder.Session, results: any, next?: (results?: builder.IDialogResult<any>) => void) => {
-        let medication: IStoredData = session.dialogData.drug;
+        let medication: IInformationStoredData = session.dialogData.drug;
         if (results && results.response) {
             medication = session.dialogData.drug = {
                 name : results.response,
@@ -56,7 +64,7 @@ lib.dialog('information', [
         }
     },
     async (session: builder.Session, results: any, next?: (results?: builder.IDialogResult<any>) => void) => {
-        let medication: IStoredData = session.dialogData.drug;
+        let medication: IInformationStoredData = session.dialogData.drug;
 
         // Formatting the response
         if (results && results.response && results.response.entity && medication.search) {
@@ -84,6 +92,80 @@ lib.dialog('information', [
         session.endDialog();
     }
 ]).triggerAction({ matches: 'Intent.Medications.GetInformation' });
+
+lib.dialog('translate', [
+    async (session: builder.Session, args: any, next?: (results?: builder.IDialogResult<any>) => void) => {
+        let data: ITranslateStoredData|undefined = undefined;
+
+        // Get the Medication entity
+        if (args && args.intent) {
+            let intent = args.intent;
+            let medicationEntity: IEntityEx = builder.EntityRecognizer.findEntity(intent.entities, 'Medication.Name') as IEntityEx;
+            let languageEntity: IEntityEx = builder.EntityRecognizer.findEntity(intent.entities, 'Language') as IEntityEx;
+
+            data = session.dialogData.drug = {
+                name: medicationEntity ? medicationEntity.entity : undefined,
+                language: languageEntity ? languageEntity.entity : undefined,
+                languageCode: languageEntity ? languageEntity.resolution.values[0] : undefined,
+                search : undefined
+            }
+        }
+
+        if (data && data.name) {
+            if (next) next();
+        }
+        else {
+            builder.Prompts.text(session, "What is the medication name ?")
+        }
+    },
+    async (session: builder.Session, results: any, next?: (results?: builder.IDialogResult<any>) => void) => {
+        
+        if (results && results.response) {
+            session.dialogData.drug.name = results.response;
+        }
+        let data: ITranslateStoredData = session.dialogData.drug;
+
+        if (data && data.name) {
+            try {
+                let listOfMedication = await openmedicament.getMedicationCodeFromQueryAsync(data.name);
+                medicationPrompt(session, listOfMedication, next);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    },
+    async (session: builder.Session, results: any, next?: (results?: builder.IDialogResult<any>) => void) => {
+        let data: ITranslateStoredData = session.dialogData.drug;
+
+        // Formatting the response
+        if (results && results.response && results.response.entity && data.search) {
+            let response = results.response.entity;
+
+            for (let item of data.search) {
+                if (item.denomination == response) {
+                    let drug = await openmedicament.getMedicationFromIdAsync(item.codeCIS);
+
+                    session.send("This medicine contains the following active substances:");
+
+                    for(var composition of drug.compositions) {
+                        for(var substance of composition.substancesActives) {
+                            let translatedSubstance = await translator.getTranslationAsync(substance.denominationSubstance, 'fr', data.languageCode);
+                            
+                            if (translatedSubstance) {
+                                let card = medicationcard.getMedicineCard(substance.denominationSubstance.toLowerCase(), translatedSubstance);
+
+                                let message = new builder.Message(session).addAttachment(card);
+                                session.send(message);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        session.endDialog();
+    }
+]).triggerAction({ matches: 'Intent.Medications.Translate' });
 
 
 function medicationPrompt(session: builder.Session, search: MedicationCode[], next?: (results?: builder.IDialogResult<any>) => void) {
--- out/dialogs/medication-dialog.js
+++ out/dialogs/medication-dialog.js
@@ -11,6 +11,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const builder = require("botbuilder");
 const api_openmedicaments_1 = require("../services/api-openmedicaments");
 const cognitive_translator_1 = require("../services/cognitive-translator");
+const medication_card_1 = require("../cards/medication-card");
 let lib = new builder.Library('medication');
 lib.dialog('information', [
     (session, args, next) => __awaiter(this, void 0, void 0, function* () {
@@ -74,6 +75,69 @@ lib.dialog('information', [
         session.endDialog();
     })
 ]).triggerAction({ matches: 'Intent.Medications.GetInformation' });
+lib.dialog('translate', [
+    (session, args, next) => __awaiter(this, void 0, void 0, function* () {
+        let data = undefined;
+        // Get the Medication entity
+        if (args && args.intent) {
+            let intent = args.intent;
+            let medicationEntity = builder.EntityRecognizer.findEntity(intent.entities, 'Medication.Name');
+            let languageEntity = builder.EntityRecognizer.findEntity(intent.entities, 'Language');
+            data = session.dialogData.drug = {
+                name: medicationEntity ? medicationEntity.entity : undefined,
+                language: languageEntity ? languageEntity.entity : undefined,
+                languageCode: languageEntity ? languageEntity.resolution.values[0] : undefined,
+                search: undefined
+            };
+        }
+        if (data && data.name) {
+            if (next)
+                next();
+        }
+        else {
+            builder.Prompts.text(session, "What is the medication name ?");
+        }
+    }),
+    (session, results, next) => __awaiter(this, void 0, void 0, function* () {
+        if (results && results.response) {
+            session.dialogData.drug.name = results.response;
+        }
+        let data = session.dialogData.drug;
+        if (data && data.name) {
+            try {
+                let listOfMedication = yield api_openmedicaments_1.default.getMedicationCodeFromQueryAsync(data.name);
+                medicationPrompt(session, listOfMedication, next);
+            }
+            catch (error) {
+                console.error(error);
+            }
+        }
+    }),
+    (session, results, next) => __awaiter(this, void 0, void 0, function* () {
+        let data = session.dialogData.drug;
+        // Formatting the response
+        if (results && results.response && results.response.entity && data.search) {
+            let response = results.response.entity;
+            for (let item of data.search) {
+                if (item.denomination == response) {
+                    let drug = yield api_openmedicaments_1.default.getMedicationFromIdAsync(item.codeCIS);
+                    session.send("This medicine contains the following active substances:");
+                    for (var composition of drug.compositions) {
+                        for (var substance of composition.substancesActives) {
+                            let translatedSubstance = yield cognitive_translator_1.default.getTranslationAsync(substance.denominationSubstance, 'fr', data.languageCode);
+                            if (translatedSubstance) {
+                                let card = medication_card_1.default.getMedicineCard(substance.denominationSubstance.toLowerCase(), translatedSubstance);
+                                let message = new builder.Message(session).addAttachment(card);
+                                session.send(message);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        session.endDialog();
+    })
+]).triggerAction({ matches: 'Intent.Medications.Translate' });
 function medicationPrompt(session, search, next) {
     let medications = new Array();
     if (search && search.length > 0) {
